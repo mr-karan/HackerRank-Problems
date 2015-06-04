77ce59ae9a47feb92502d02a392a5fb2bbdfe933
Karan Sharma <karansharma1295@gmail.com>
2015-06-04 23:43:11 +0530

    Problems added

---
+++ Algorithms/Warmup/CutTheStick.py
@@ -0,0 +1,19 @@
+'''
+sticks-length        length-of-cut   sticks-cut
+5 4 4 2 2 8             2               6
+3 2 2 _ _ 6             2               4
+1 _ _ _ _ 4             1               2
+_ _ _ _ _ 3             3               1
+_ _ _ _ _ _           DONE            DONE
+
+'''
+n = input()
+sticks = map(int, raw_input().strip().split(" "))
+
+
+sticks=sorted(sticks,reverse=True)
+while(len(sticks)>0):
+    print len(sticks)
+    leastnum=sticks.pop()
+    while len(sticks)>0 and sticks[-1]<=leastnum:
+        sticks.pop()
--- Algorithms/Warmup/MaximizingXOR.py
+++ Algorithms/Warmup/MaximizingXOR.py
@@ -1,12 +1,12 @@
-l=input()
+l = input()
 r = input()
 
-ans=[]
+ans = []
 
-for i in range(l,r):
-	for j in range(l,r):
-		ans.append(i^j)
+for i in range(l, r):
+    for j in range(l, r):
+        ans.append(i ^ j)
 
-ans=sorted(ans,reverse=True)
+ans = sorted(ans, reverse=True)
 
 print ans[0]
