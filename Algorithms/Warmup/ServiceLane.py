1359823e737084fea98745ec88632b0d578ef9da
Karan Sharma <karansharma1295@gmail.com>
2015-06-05 23:01:58 +0530

    Day5

---
+++ Algorithms/Warmup/ServiceLane.py
@@ -0,0 +1,13 @@
+
+testcase = map(int, raw_input().strip().split(" "))
+width = map(int, raw_input().strip().split(" "))
+
+for x in range(testcase[1]+1):
+	
+	segment = map(int, raw_input().strip().split(" "))
+
+	allowed = []
+	for i in range(segment[0], segment[1] + 1):
+		allowed.append(width[i])
+
+	print min(allowed)
