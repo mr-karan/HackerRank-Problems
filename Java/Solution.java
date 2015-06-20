ed67a76b28b57b0a17cce81ea82d20f00521de13
Karan Sharma <karansharma1295@gmail.com>
2015-06-20 17:18:18 +0530

    Java

---
+++ Java/Solution.java
@@ -0,0 +1,8 @@
+//Write your code here.
+//Name of your public class should be 'Solution'.
+public class Solution {
+   
+   public static void main(String []args) {
+      System.out.println("Hello World.\nHello Java.");
+   }
+} 
