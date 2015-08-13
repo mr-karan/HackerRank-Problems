d821e1d87599acb67c4b646e046696a2f6cf5709
Karan Sharma <karansharma1295@gmail.com>
2015-08-14 00:26:23 +0530

    Create bigint.java

---
+++ Java/bigint.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+                  BigInteger a = sc.nextBigInteger();            
+                         BigInteger b = sc.nextBigInteger();            
+        BigInteger sum=a.add(b);
+        BigInteger prod=a.multiply(b);
+        System.out.println(sum);
+        System.out.println(prod);
+        
+        
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
